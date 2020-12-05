b7b4d1419aa08cedf38c00fef43d2d7e332d0975
agastyamishra <74876509+agastyamishra@users.noreply.github.com>
2020-12-05 12:47:39 -0500

    sAHGS

--- Universal-files/script.js
+++ Universal-files/script.js
@@ -1,1 +1,10 @@
-//test
+
+function switchSheet() {
+    let theme = document.getElementById("theme");
+
+    if (theme.getAttribute("href") == "Universal-files/style.css") {
+        theme.href = "Universal-files/darkstyle.css";
+    } else {
+        theme.href = "Universal-files/style.css";
+    }
+}
