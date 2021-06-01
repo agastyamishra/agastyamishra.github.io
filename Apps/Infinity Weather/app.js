20e181f78d53299e744de7d82961d04a2ffc78f1
agastyamishra <74876509+agastyamishra@users.noreply.github.com>
2021-06-01 17:52:22 -0400

    Installed Infinity Weather to website directory and bug fixes
    Launched the first beta of Infinity Weather on the website directory. Made some bug fixes to style.css on the main website. Also uploaded Infinity Recipes Beta 1 to website.

---
+++ Apps/Infinity Weather/app.js
@@ -0,0 +1,48 @@
+function webWidth (){
+    if (screen.availWidth < 320) {
+        alert("This website may not work properly with the amount of scrren space your device has")
+    }
+}
+
+
+webWidth();
+
+var cityName = "Louisville"
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var weather = JSON.parse(this.responseText);
+    var weatherTemp = weather.main;
+    console.log(weatherTemp);
+    var floatTemp = weatherTemp.temp
+    var trimmedTemp = parseInt(floatTemp)
+    document.getElementById("current_temp").innerHTML = trimmedTemp + "°C";
+  }
+};
+xmlhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=ae57b07a14d0843eb4fa4e214552bc52&&units=metric", true);
+xmlhttp.send();
+
+
+function newCity() {
+  cityName = document.getElementById("addCityText").value;
+  console.log(cityName)
+  document.getElementById("location_city").innerHTML = cityName
+  document.getElementById("current_temp").style.fontSize = "130px"
+  //NewCity Callback
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var weather = JSON.parse(this.responseText);
+    var weatherTemp = weather.main;
+    console.log(weatherTemp);
+    var floatTemp = weatherTemp.temp
+    var trimmedTemp = parseInt(floatTemp)
+    document.getElementById("current_temp").innerHTML = trimmedTemp + "°C";
+      } else if (this.status == 404){
+      document.getElementById("current_temp").innerHTML = "Error while loading weather information" 
+      document.getElementById("current_temp").style.fontSize = "30px" }
+  };
+  xmlhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=ae57b07a14d0843eb4fa4e214552bc52&&units=metric", true);
+  xmlhttp.send();
+
+}
